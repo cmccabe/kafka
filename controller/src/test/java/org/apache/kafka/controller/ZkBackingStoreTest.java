07e459142d6035a04ce61273d31251764d1a98c5
Colin P. Mccabe <cmccabe@confluent.io>
2020-04-17 17:37:59 -0700

    Add ZkBackingStoreTest#testOnlyOneActivates

--- controller/src/test/java/org/apache/kafka/controller/ZkBackingStoreTest.java
+++ controller/src/test/java/org/apache/kafka/controller/ZkBackingStoreTest.java
@@ -20,11 +20,20 @@ package org.apache.kafka.controller;
 import kafka.zk.BrokerInfo;
 import kafka.zk.ZooKeeperTestHarness;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.kafka.controller.BackingStore.ActivationListener;
+import org.apache.kafka.test.TestUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
+
+import static org.junit.Assert.assertEquals;
 
 public class ZkBackingStoreTest extends ZooKeeperTestHarness {
     @Rule
@@ -33,30 +42,91 @@ public class ZkBackingStoreTest extends ZooKeeperTestHarness {
     @Test
     public void testCreateAndClose() throws Exception {
         zkClient().createTopLevelPaths();
-        ZkBackingStore store = ZkBackingStore.create(0, "", zkClient());
-        store.close();
+        try (ZkBackingStore store = ZkBackingStore.create(0, "", zkClient())) {
+        }
     }
 
     @Test
     public void testStartAndClose() throws Exception {
         zkClient().createTopLevelPaths();
-        ZkBackingStore store = ZkBackingStore.create(0, "", zkClient());
-        BrokerInfo broker0Info = ControllerTestUtils.newBrokerInfo(0);
-        final CountDownLatch hasActivated = new CountDownLatch(1);
-        CompletableFuture<Void> startFuture =
-            store.start(broker0Info, new BackingStore.ActivationListener() {
-                @Override
-                public void activate(KafkaController newController) {
-                    hasActivated.countDown();
-                }
+        try (ZkBackingStore store = ZkBackingStore.create(0, "", zkClient())) {
+            BrokerInfo broker0Info = ControllerTestUtils.newBrokerInfo(0);
+            final CountDownLatch hasActivated = new CountDownLatch(1);
+            CompletableFuture<Void> startFuture =
+                store.start(broker0Info, new ActivationListener() {
+                    @Override
+                    public void activate(KafkaController newController) {
+                        hasActivated.countDown();
+                    }
 
-                @Override
-                public void deactivate() {
+                    @Override
+                    public void deactivate() {
+                    }
+                });
+            startFuture.get();
+            hasActivated.await();
+        }
+    }
+
+    private class ZkBackingStoreTestContext implements ActivationListener {
+        private final ZkBackingStore store;
+        private final int index;
+        private final AtomicBoolean active = new AtomicBoolean(false);
+
+        ZkBackingStoreTestContext(int index) {
+            this.store = ZkBackingStore.create(index, "", zkClient());
+            this.index = index;
+        }
+
+        CompletableFuture<Void> start() {
+            return store.start(ControllerTestUtils.newBrokerInfo(index), this);
+        }
+
+        @Override
+        public void activate(KafkaController newController) {
+            active.set(true);
+        }
+
+        @Override
+        public void deactivate() {
+            active.set(false);
+        }
+    }
+
+    @Test
+    public void testOnlyOneActivates() throws Exception {
+        zkClient().createTopLevelPaths();
+
+        final int numStores = 5;
+        List<ZkBackingStoreTestContext> storeContexts = new ArrayList<>(numStores);
+        try {
+            for (int i = 0; i < numStores; i++) {
+                storeContexts.add(new ZkBackingStoreTestContext(i));
+            }
+            List<CompletableFuture<Void>> startFutures = new ArrayList<>();
+            for (ZkBackingStoreTestContext context : storeContexts) {
+                startFutures.add(context.start());
+            }
+            for (CompletableFuture<Void> startFuture : startFutures) {
+                startFuture.get();
+            }
+            TestUtils.waitForCondition(() -> {
+                int numActive = 0;
+                for (ZkBackingStoreTestContext context : storeContexts) {
+                    if (context.active.get()) {
+                        numActive++;
+                    }
                 }
-            });
-        startFuture.get();
-        hasActivated.await();
-        store.close();
+                return 1 == numActive;
+            }, "waiting for a single controller to become active");
+        } finally {
+            for (ZkBackingStoreTestContext context : storeContexts) {
+                context.store.shutdown(TimeUnit.SECONDS, 0);
+            }
+            for (ZkBackingStoreTestContext context : storeContexts) {
+                context.store.close();
+            }
+        }
     }
 
 //    @Test
